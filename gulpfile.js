b5029534e291df3ce7ad223d4406384c45dbb530
Sean Stansell <seanstansell@gmail.com>
2015-06-23 13:18:44 -0400

    major updates

--- gulpfile.js
+++ gulpfile.js
@@ -12,16 +12,15 @@ gulp.task('serve', ['sass'], function() {
       baseDir: 'dev'
     }
   });
-  gulp.watch("dev/sass/*.scss", ['sass']);
-  gulp.watch(['*.html', 'css/**/*.css', 'js/**/*.js']).on('change', browserSync.reload);
+  gulp.watch("dev/sass/style.scss", ['sass']);
+  //gulp.watch(['*.html', 'css/**/*.css', 'js/**/*.js'], browserSync.reload);
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'dev'}, reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("dev/sass/*.scss")
-        .pipe(sass({
-        	outputStyle: 'compressed'
-        	}))
+    return gulp.src("dev/sass/style.scss")
+        .pipe(sass())
         .pipe(gulp.dest("dev/css"))
         .pipe(browserSync.stream());
 });
